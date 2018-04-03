f0cb4c07708e80af5479461d5e7552e3298df3e9
PoetaKodu <razzorflame@o2.pl>
2018-04-04 00:16:49 +0200

    Added basic analysis and request interface.

---
+++ SignalProcessor/Analyzer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalProcessor
+{
+    namespace Analyze
+    {
+        /// <summary>
+        /// An interface for every data analytic methods.
+        /// </summary>
+        interface IAnalyzer
+        {
+            /// <summary>
+            /// Analyzes data using the specified request parameters.
+            /// </summary>
+            /// <param name="request_">The request.</param>
+            /// <returns>Analyzed data.</returns>
+            IAnalyzedData analyze(IRequest request_);
+        }
+    }
+}
---
+++ SignalProcessor/Request.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalProcessor
+{
+    namespace Analyze
+    {
+        /// <summary>
+        /// Analysis settings for <see cref="IAnalyzer"/>.
+        /// </summary>
+        interface IRequest
+        {
+        }
+    }
+}
