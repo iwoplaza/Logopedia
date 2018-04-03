0cf4e45c0c82ab9c61586c139f4dc0db3dd301cf
PoetaKodu <razzorflame@o2.pl>
2018-04-03 22:35:58 +0200

    Converted /**/ comment to XML.

--- SignalProcessor/RawDataContainer.cs
+++ SignalProcessor/RawDataContainer.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace SignalProcessor
 {
-    /*
-        Contains a portion of data not yet analized, but presented
-        in the most simplistic way. 
-    */
+    /// <summary>
+    /// Contains a portion of data not yet analized, but presented
+    /// in the most simplistic way.
+    /// </summary>
     class RawDataContainer
     {
     }
