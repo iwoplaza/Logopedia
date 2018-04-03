394377a7c059b8d3a5fe5cdef5152d3512d96a89
Iwo Plaza <gobbobpl@gmail.com>
2018-04-03 21:50:46 +0200

    Added an IOManager, and a RawDataContainer

---
+++ SignalProcessor/IOManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalProcessor
+{
+    class IOManager
+    {
+        /*
+            Loads the specified amount of samples 
+        */
+        public RawDataContainer LoadFileSnippet(String filePath, uint samples) { return null; }
+    }
+}
---
+++ SignalProcessor/RawDataContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalProcessor
+{
+    /*
+        Contains a portion of data not yet analized, but presented
+        in the most simplistic way. 
+    */
+    class RawDataContainer
+    {
+    }
+}
