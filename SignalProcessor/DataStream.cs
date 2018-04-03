28ad37aef25e36eb428b345b44c0fcb141d1d33b
Iwo Plaza <gobbobpl@gmail.com>
2018-04-03 22:15:15 +0200

    Renamed the IOManager to DataStream, to better reflect it's usage.

---
+++ SignalProcessor/DataStream.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalProcessor
+{
+    class DataStream
+    {
+        /*
+            Creates a DataStream that can be used to stream data from a file specified
+            by the filePath parameter.
+        */
+        DataStream(String filePath) {}
+        /*
+            Returns the specified amount of samples from the file specified by the
+            filePath, and advances the reader by that same amount.
+        */
+        public RawDataContainer GetNextSnippet(uint samples) { return null; }
+    }
+}
